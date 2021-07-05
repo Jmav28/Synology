63e20956caaa4f88fc13c1e0a8516933a0d9a87e
Miguel Avila <miguelavilaa28@gmail.com>
2021-07-05 08:55:13 -0700

    primer avance de navbar con js

---
+++ main.js
@@ -0,0 +1,31 @@
+const btnNav = document.getElementById('btn-nav-enlaces'),
+    grid = document.getElementById('grid');
+
+document.querySelectorAll('#container-nav .enlaces a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+        
+        grid.classList.add('activo');
+
+        grid.addEventListener('mouseleave', () => {
+            grid.classList.remove('activo');
+        });
+
+		document.querySelectorAll('#grid .categoria-menu').forEach((categoria) => {
+            categoria.classList.remove('activo');
+            if(categoria.dataset.categoria == e.target.dataset.categoria){
+                categoria.classList.add('activo');
+            }
+        });
+	});
+});
+
+document.querySelectorAll('#container-subcategorias .enlaces-subcategorias-cont a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+        document.querySelectorAll('.container-subcategorias .banner-subcategorias .container-subcategorias-img').forEach((categoria) => {
+            categoria.classList.remove('activo');
+            if(categoria.dataset.categoria == e.target.dataset.categoria){
+                categoria.classList.add('activo');
+            }
+        });
+	});
+});
