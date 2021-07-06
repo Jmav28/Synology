3972c67c97ee10d235579479adc0e59580ad2057
Miguel Avila <miguelavilaa28@gmail.com>
2021-07-05 18:20:56 -0700

    navbarr corregida hasta products

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
 const btnNav = document.getElementById('btn-nav-enlaces'),
     grid = document.getElementById('grid');
 
-document.querySelectorAll('#container-nav .enlaces a').forEach((elemento) => {
-	elemento.addEventListener('mouseenter', (e) => {
-        
-        grid.classList.add('activo');
+btnNav.addEventListener('mouseover', () => {
+    grid.classList.add('activo');
 
-        grid.addEventListener('mouseleave', () => {
-            grid.classList.remove('activo');
+    document.querySelectorAll('.container-nav .enlaces a').forEach((elemento) => {
+        elemento.addEventListener('mouseenter', (e) => {
+            document.querySelectorAll('.container-grid .grid .categoria-menu').forEach((categoria) => {
+                categoria.classList.remove('activo');
+                if(categoria.dataset.categoria == e.target.dataset.categoria){
+                    categoria.classList.add('activo');
+                }
+            });
         });
+    });
 
-		document.querySelectorAll('#grid .categoria-menu').forEach((categoria) => {
+    grid.addEventListener('mouseleave', () => {
+        grid.classList.remove('activo');
+    });
+});
+
+document.querySelectorAll('.grid .enlaces a').forEach((elemento) => {
+    elemento.addEventListener('mouseenter', (e) => {
+        document.querySelectorAll('#grid .categoria-menu3 .container-subcategorias').forEach((categoria) => {
             categoria.classList.remove('activo');
             if(categoria.dataset.categoria == e.target.dataset.categoria){
                 categoria.classList.add('activo');
             }
         });
-	});
+    });
 });
 
-document.querySelectorAll('#container-subcategorias .enlaces-subcategorias-cont a').forEach((elemento) => {
-	elemento.addEventListener('mouseenter', (e) => {
-        document.querySelectorAll('.container-subcategorias .banner-subcategorias .container-subcategorias-img').forEach((categoria) => {
+document.querySelectorAll('#grid .container-subcategorias .enlaces-subcategorias-cont a').forEach((elemento) => {
+    elemento.addEventListener('mouseenter', (e) => {
+        document.querySelectorAll('#grid .banner-subcategorias .container-subcategorias-img').forEach((categoria) => {
             categoria.classList.remove('activo');
-            if(categoria.dataset.categoria === e.target.dataset.categoria){
+            if(categoria.dataset.categoria == e.target.dataset.categoria){
                 categoria.classList.add('activo');
             }
         });
-	});
-});
-
-document.querySelectorAll('#grid .enlaces a').forEach((elemento) => {
-	elemento.addEventListener('mouseenter', (e) => {
-        document.querySelectorAll('.container-subcategorias').forEach((categoria) => {
-            categoria.classList.remove('activo');
-            if(categoria.dataset.categoria === e.target.dataset.categoria){
-                categoria.classList.add('activo');
-            }
-        });
-	});
+    });
 });
